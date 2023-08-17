5ba8fca48a87b3273ec1889d8d9860cd10574e7b
Tibor Schneider <sctibor@ethz.ch>
2023-08-17 13:32:50 +0200

    Update

--- bgpsim-web-eaf0fe8b25d62fb5.js
+++ bgpsim-web-eaf0fe8b25d62fb5.js
@@ -1009,7 +1009,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('bgpsim-web-28f57459ad4729de_bg.wasm', import.meta.url);
+        input = new URL('bgpsim-web-eaf0fe8b25d62fb5_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
